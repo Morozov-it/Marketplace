c4c5cea15c77fbd6842add3eead673f156c20266
Morozov-it <morozov.it2020@gmail.com>
2022-02-28 11:21:03 +0300

    Connect PostgreSQL db, config .env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,4 +8,16 @@ const PORT = process.env.PORT || 5000;
 //запуск приложения
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+    try {
+        //запуск авторизации к бд
+        await sequelize.authenticate();
+        //сверка состояния данных со схема для базы данных
+        await sequelize.sync()
+        //запуск приложения
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e)
+    }
+};
+start();
