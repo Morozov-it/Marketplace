95d022b585e02eed3f5b4fc070962888970c984a
Morozov-it <morozov.it2020@gmail.com>
2022-03-01 10:46:04 +0300

    Create registration users via JWT

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,49 @@
-const ApiError = require("../error/apiError")
+const ApiError = require("../error/apiError");
+const bcrypt = require('bcrypt');
+const {User, Basket} = require('../models/models');
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (id, email, role) => {
+    return jwt.sign(
+        { id, email, role }, //payload
+        process.env.SECRET_KEY, //key
+        { expiresIn: '24h' } //options
+    );
+}
 
 class UserController {
     async registration(req, res) {
-        try {
-
-        } catch (e) {
-            console.log(e)
+        //получение рег.данных из тела запроса
+        const { email, password, role } = req.body;
+        
+        //проверка ввода данных
+        if (!email || !password) {
+            return next(ApiError.badRequest("Email or password is empty"))
         }
+
+        //проверка существующего пользователя
+        const candidate = await User.findOne({ where: { email } });
+        if (candidate) {
+            return next(ApiError.badRequest("This user is existed"))
+        }
+
+        //шифрование пароля
+        const hashPassword = await bcrypt.hash(password, 3);
+
+        //создание пользователя в бд
+        const user = await User.create({ email, role, password: hashPassword });
+        //создание корзины пользователя в бд
+        const basket = await Basket.create({ userId: user.id });
+
+        //создание токена для пользователя
+        const token = generateJWT(user.id, user.email, user.role);//role: default "USER"
+        
+        //возврат токена на клиент
+        return res.json({token})
     }
 
     async login(req, res) {
-        try {
 
-        } catch (e) {
-            console.log(e)
-        }
     }
 
     async check(req, res, next) {
